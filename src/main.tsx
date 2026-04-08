e7b73a6da56a01019b94f52285049b5d4c5e6065
FarhadKey <farhad.keyanvash@momentum-glass.com>
2026-04-08 16:33:10 -0500

    Fix: handle MSAL redirect response before rendering app

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,9 @@ import './components/ui/ui.css'
 import App from './App.tsx'
 
 msalInstance.initialize().then(() => {
+  // Process the login redirect response before rendering
+  return msalInstance.handleRedirectPromise();
+}).then(() => {
   createRoot(document.getElementById('root')!).render(
     <StrictMode>
       <MsalProvider instance={msalInstance}>
